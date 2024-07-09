fa9a3a09de9720f4aa75e11d4fb112dc9781a804
mateusmaiamaia <mateusmaiamaia@hotmail.com>
2024-07-09 09:53:39 -0300

    Add SQL script to setup properties_df database

---
+++ data/setup_properties_df_db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE properties_df;
+
+\c properties_df;
+
+CREATE TABLE properties (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    price VARCHAR(50),
+    sqm_price VARCHAR(50),
+    area VARCHAR(50)
+);
+
